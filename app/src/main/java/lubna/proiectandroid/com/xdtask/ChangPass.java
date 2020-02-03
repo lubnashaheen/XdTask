6e2de5d9c893ef0b4ea77097d03dc8a028bd2877
lubnashaheen <loloshaheen777@gmail.com>
2020-02-03 12:41:35 +0300

    initial commit

---
+++ app/src/main/java/lubna/proiectandroid/com/xdtask/ChangPass.java
@@ -0,0 +1,14 @@
+package lubna.proiectandroid.com.xdtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chang_pass);
+    }
+}
